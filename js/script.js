21032276f161d3a3157a5530576be149fe066f41
ellavrubel <webella@protonmail.com>
2019-12-26 11:33:32 +0300

    scroll popup slider

--- js/script.js
+++ js/script.js
@@ -19,10 +19,65 @@ $(document).ready(function () {
 
 //    меню Hamburger
 
-    $('.menu__toggle').click(function (e) {
-        e.preventDefault();
-        $('li.header__menu').toggleClass('hamb');
+    if( $(window).width() < 768) {
 
+        $('.menu__toggle').click(function (e) {
+            e.preventDefault();
+            $('li.header__menu').toggleClass('hamb');
+        });
+        $('ul.header__nav li a').click(function (e) {
+            e.preventDefault();
+            $('li.header__menu').toggleClass('hamb');
+        });
+    }
+
+//    плавная прокрутка
+
+    $("a.scrollto").click(function() {
+        var elementClick = $(this).attr("href");
+        var destination = $(elementClick).offset().top;
+        jQuery("html:not(:animated),body:not(:animated)").animate({
+            scrollTop: destination }, 800);
+        return false;
+    });
+
+//    Pop-up window
+
+    $('a.modal-btn').click(function (e) {
+        e.preventDefault();
+        var destinationPopup = $(this).attr('href');
+        $(destinationPopup).addClass('window');
+
+        $('.popup__close, .popup__overlay').click(function () {
+        $(destinationPopup).removeClass('window');
+        });
+    });
+
+//    slider
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        slideSpeed : 500,
+        center: true,
+        itemsScaleUp:true,
+        autoHeight:true,
+
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
     })
+
+
+
+
 
 });
